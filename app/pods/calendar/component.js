fc188dba51b779dfe7a2abbae5a9696a346a9858
Chuck Grimmett <chuck@grimmett.co>
2019-12-04 22:11:26 -0500

    responsive chart

--- app/pods/calendar/component.js
+++ app/pods/calendar/component.js
@@ -1,6 +1,6 @@
 import Component from "@ember/component";
 import { scaleTime, scaleBand } from "d3-scale";
-import { select } from "d3-selection";
+import { select, selectAll } from "d3-selection";
 import { axisBottom } from "d3-axis";
 import { timeFormat } from "d3-time-format";
 import { timeWeek, timeDay } from "d3-time";
@@ -45,69 +45,83 @@ function sortDateAscending(a, b) {
 data = data.sort(sortDateAscending);
 let lastItem = data.length - 1;
 
-export default class Calendar extends Component {
-  didInsertElement() {
-    let svg = select(this.$("svg")[0]);
-    let margin = { top: 30, right: 0, bottom: 10, left: 30 };
-    let height = data.length * 25 + margin.top + margin.bottom;
-    let width = 1200;
+function drawChart() {
+  let svg = select("#calendar-container")
+    .append("svg")
+    .attr("width", "100%")
+    .attr("height", "100%")
+    .attr("id", "calendar");
+  let margin = { top: 30, right: 0, bottom: 10, left: 30 };
+  let height = data.length * 25 + margin.top + margin.bottom;
 
-    let x = scaleTime()
-      .domain([
-        timeDay.offset(new Date(data[0].start), -15),
-        timeDay.offset(
-          new Date(data[lastItem].start),
-          data[lastItem].daysToMaturity + 15
-        )
-      ])
-      .range([10, width - 10]);
+  let width = svg.node().getBoundingClientRect().width;
 
-    let y = scaleBand()
-      .domain(data.map(d => d.name))
-      .range([margin.top, height - margin.bottom])
-      .padding(0.1);
+  let x = scaleTime()
+    .domain([
+      timeDay.offset(new Date(data[0].start), -15),
+      timeDay.offset(
+        new Date(data[lastItem].start),
+        data[lastItem].daysToMaturity + 15
+      )
+    ])
+    .range([10, width - 10]);
 
-    svg.append("g").call(
-      axisBottom()
-        .scale(x)
-        .ticks(timeWeek.every())
-        .tickFormat(timeFormat("%b %d"))
+  let y = scaleBand()
+    .domain(data.map(d => d.name))
+    .range([margin.top, height - margin.bottom])
+    .padding(0.1);
+
+  svg.append("g").call(
+    axisBottom()
+      .scale(x)
+      .ticks(timeWeek.every())
+      .tickFormat(timeFormat("%b %d"))
+  );
+
+  svg
+    .append("g")
+    .selectAll("rect")
+    .data(data)
+    .join("rect")
+    .attr("x", d => x(new Date(d.start)))
+    .attr("height", y.bandwidth())
+    .attr("y", d => y(d.name))
+    .style("fill", d => d.color)
+    .attr("rx", "5")
+    .attr(
+      "width",
+      d =>
+        x(timeDay.offset(new Date(d.start), d.daysToMaturity)) -
+        x(new Date(d.start))
     );
 
-    svg
-      .append("g")
-      .selectAll("rect")
-      .data(data)
-      .join("rect")
-      .attr("x", d => x(new Date(d.start)))
-      .attr("height", y.bandwidth())
-      .attr("y", d => y(d.name))
-      .style("fill", d => d.color)
-      .attr("rx", "5")
-      .attr(
-        "width",
-        d =>
-          x(timeDay.offset(new Date(d.start), d.daysToMaturity)) -
-          x(new Date(d.start))
-      );
+  svg
+    .append("g")
+    .attr("fill", "white")
+    .attr("text-anchor", "middle")
+    .style("font", "12px sans-serif")
+    .selectAll("text")
+    .data(data)
+    .join("text")
+    .attr(
+      "x",
+      d =>
+        (x(timeDay.offset(new Date(d.start), d.daysToMaturity)) +
+          x(new Date(d.start))) /
+        2
+    )
+    .attr("y", d => y(d.name) + y.bandwidth() / 2)
+    .attr("dy", "0.35em")
+    .text(d => d.name);
+}
 
-    svg
-      .append("g")
-      .attr("fill", "white")
-      .attr("text-anchor", "middle")
-      .style("font", "12px sans-serif")
-      .selectAll("text")
-      .data(data)
-      .join("text")
-      .attr(
-        "x",
-        d =>
-          (x(timeDay.offset(new Date(d.start), d.daysToMaturity)) +
-            x(new Date(d.start))) /
-          2
-      )
-      .attr("y", d => y(d.name) + y.bandwidth() / 2)
-      .attr("dy", "0.35em")
-      .text(d => d.name);
+export default class Calendar extends Component {
+  didInsertElement() {
+    drawChart();
+
+    window.addEventListener("resize", function() {
+      selectAll("svg").remove();
+      drawChart();
+    });
   }
 }
